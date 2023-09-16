e74c7d78f422392841583ec9fc2d36d8b56f69ca
Wilberto Morales <wilbertomorales777@gmail.com>
2023-09-15 20:48:47 -0500

    Add display class

---
+++ include/display.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+class Display {
+public:
+    Display(int width, int height, int rows, int columns);
+
+private:
+    int width;
+    int height;
+    int rows;
+    int columns;
+};
---
+++ src/display.cpp
@@ -0,0 +1,8 @@
+#include "display.hpp"
+
+Display::Display(int width, int height, int rows, int columns) {
+    this->width = width;
+    this->height = height;
+    this->rows = rows;
+    this->columns = columns;
+}
