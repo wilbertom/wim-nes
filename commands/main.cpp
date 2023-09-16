3160a2b411585d58e0e597ba05d6cf8cba60246e
Wilberto Morales <wilbertomorales777@gmail.com>
2023-09-15 20:53:48 -0500

    Add index header

--- commands/main.cpp
+++ commands/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 
+#include "wimnes.hpp"
+
 int main() {
     std::cout << "Test " << std::endl;
+    std::cout << pale_turquoise.r << " " << pale_turquoise.g << " " << pale_turquoise.b << std::endl;
 
     return 0;
 }
---
+++ include/wimnes.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "color.hpp"
+#include "display.hpp"
+#include "easy6502.hpp"
+#include "palette.hpp"
