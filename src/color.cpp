66088fd2b69e147f6330ea1a81d8320296abafc4
Wilberto Morales <wilbertomorales777@gmail.com>
2023-09-16 01:21:02 -0500

    Rename RGB to Color

--- src/color.cpp
+++ src/color.cpp
@@ -1,12 +1,12 @@
-#include "rgb.hpp"
+#include "color.hpp"
 
-RGB::RGB(int r, int g, int b) {
+Color::Color(int r, int g, int b) {
     this->r = r;
     this->g = g;
     this->b = b;
 }
 
-RGB::RGB(int hex) {
+Color::Color(int hex) {
     this->r = (hex >> 16);
     this->g = (hex >> 8) & 0x00ff;
     this->b = hex & 0x0000ff;
