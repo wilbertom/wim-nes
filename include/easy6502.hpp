a9054fe7021801f23cd6c886678232010226bee4
Wilberto Morales <wilbertomorales777@gmail.com>
2023-09-15 20:37:47 -0500

    Add color palette from easy6502

---
+++ include/easy6502.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "color.hpp"
+
+Color black = Color(0x000000);
+Color white = Color(0xffffff);
+Color dark_red = Color(0x880000);
+Color pale_turquoise = Color(0xaaffee);
+Color fuchsia = Color(0xcc44cc);
+Color malachite = Color(0x00cc55);
+Color new_midnight_blue = Color(0x0000aa);
+Color dolly = Color(0xeeee77);
+Color whiskey_sour = Color(0xdd8855);
+Color raw_umber = Color(0x664400);
+Color salmon = Color(0xff7777);
+Color night_rider = Color(0x333333);
+Color grey = Color(0x777777);
+Color conifer = Color(0xaaff66);
+Color dodger_blue = Color(0x0088ff);
+Color silver = Color(0xbbbbbb);
